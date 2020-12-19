f0a2ba03ecc4f522dc41c03552324f7df48badfe
James Sumner <james@jamesedward.co.uk>
2020-12-19 21:33:56 +0000

    Tree view just about there

---
+++ components/DataCollectionTree.js
@@ -0,0 +1,158 @@
+(function($){
+    $.fn.DataCollectionTree = function(options){
+
+        // Populate the details of a client into the rootElement specified
+        var populateClient = function(clientid, rootElement){
+            return function(data){
+
+                rootElement.empty();
+
+                // We always need two folders, one for tables
+                let tables = $("<div>");
+                tables.addClass("treeitem folder");
+                tables.text("Tables");
+                rootElement.append(tables);
+                let tableschildren = $("<div>");
+                tableschildren.addClass("treechildren");
+                tables.click(function(){
+                    tableschildren.toggle();
+                });
+                tableschildren.css("display", "none");
+                rootElement.append(tableschildren);
+
+                // And one for stored procedures
+                let procedures = $("<div>");
+                procedures.addClass("treeitem folder");
+                procedures.text("Procedures");
+                rootElement.append(procedures);
+                let procedureschildren = $("<div>");
+                procedureschildren.addClass("treechildren");
+                procedures.click(function(){
+                    procedureschildren.toggle();
+                });
+                rootElement.append(procedureschildren);
+
+                // Populate each data collection in the client
+                for(var x in data.Rows){
+                    let dc = data.Rows[x];
+
+                    if(dc.DataCollectionType === "Table"){
+                        let datacollection = $("<div>");
+                        datacollection.addClass("treeitem table");
+                        datacollection.text(dc.DataCollectionCode);
+                        tableschildren.append(datacollection);
+
+                        let datacollectionchildren = $("<div>")
+                        datacollectionchildren.addClass("treechildren");
+                        tableschildren.append(datacollectionchildren);
+                        datacollection.click(function(){
+                            if(datacollectionchildren.children().length > 0){
+                                datacollectionchildren.toggle();
+                            }
+                            else{
+                                datacollectionchildren.append("Loading...");
+                                options.getDataCollectionDescription(clientid, dc.DataCollectionCode, renderTableDescription(datacollectionchildren));
+                            }
+                        });
+                    }
+                    else if(dc.DataCollectionType === "StoredProcedure"){
+                        let datacollection = $("<div>");
+                        datacollection.addClass("treeitem table");
+                        datacollection.text(dc.DataCollectionCode);
+                        tableschildren.append(datacollection);
+
+                        let datacollectionchildren = $("<div>")
+                        datacollectionchildren.addClass("treechildren");
+                        tableschildren.append(datacollectionchildren);
+                        datacollection.click(function(){
+                            if(datacollectionchildren.children().length > 0){
+                                datacollectionchildren.toggle();
+                            }
+                            else{
+                                datacollectionchildren.append("Loading...");
+                                options.getDataCollectionDescription(clientid, dc.DataCollectionCode, renderStoredProcedureDescription(datacollectionchildren));
+                            }
+                        });
+                    }
+                }
+            }
+        }
+
+        // Takes the data collection description output and a root element, and populates it with the description
+        var renderTableDescription = function(parentElement){
+            return function(data){
+
+                parentElement.empty();
+
+                let columns = $("<div>");
+                columns.text("Columns");
+                columns.addClass("treeitem folder");
+                let columnChildren = $("<div>");
+                columnChildren.addClass("treechildren");
+                columnChildren.css("display", "none");
+                columns.click(function(){
+                    columnChildren.toggle();
+                });
+
+                for(var x in data.Definition.Columns){
+                    let col = data.Definition.Columns[x];
+
+                    let colDiv = $("<div>");
+                    colDiv.addClass("treeitem column");
+                    colDiv.text(col.DisplayName + " (" + col.DataType + ")" + (col.IsId?" ID":""));
+                    columnChildren.append(colDiv);
+                }
+
+                parentElement.append(columns);
+                parentElement.append(columnChildren);
+            }
+        }
+
+        // Takes the data collection description output and a root element and populates a stored procedure's details
+        var renderStoredProcedureDescription = function(parentElement){
+
+        }
+
+        // Add a tree root for each client
+        for(let x in options.clients){
+            let client = $("<div>");
+            client.text(options.clients[x]);
+            client.addClass("treeitem db");
+            let clientChildren = $("<div>");
+            clientChildren.addClass("treechildren");
+
+            client.click(function(){
+                clientChildren.toggle();
+            })
+
+            this.append(client);
+            this.append(clientChildren);
+
+            // Add the root folders
+            let dc = $("<div>");
+            dc.addClass("treeitem folder");
+            dc.text("Data Collections");
+            clientChildren.append(dc);
+            let dcChildren = $("<div>");
+            dcChildren.addClass("treechildren");
+            dc.click(function(){
+                if(dcChildren.children().length>0){
+                    dcChildren.toggle();
+                }
+                else{
+                    // Use the function provided to get details of the client and add it to the tree
+                    dcChildren.append("Loading...");
+                    options.getDataCollections(options.clients[x], populateClient(options.clients[x], dcChildren));
+                }
+            });
+            clientChildren.append(dcChildren);
+
+            let lists = $("<div>");
+            lists.addClass("treeitem folder");
+            lists.text("Lists");
+            clientChildren.append(lists);
+        }
+        
+        return this;
+    };
+}(jQuery));
--- javascript/browser.js
+++ javascript/browser.js
@@ -1,3 +1,58 @@
 $(function(){
-    
+
+    /*
+     * How do we get the data collections for a given client id
+     */
+    var getDataCollections = function(clientid, callback){
+        notification("Loading data collections for " + clientid);
+        var notifiedCallback = function(callback){
+            return function(data){
+                notification("Loaded " + clientid);
+                callback(data);
+            }
+        }
+        $.get("mockdata/DataCollections.json", notifiedCallback(callback));
+    }
+
+    /*
+     * Get the definition of a data collection
+     */
+    var getDataCollectionDescription = function(clientid, datacollection, callback){
+        notification("Loading " + datacollection + "...");
+        $.get("mockdata/DataCollectionDescription.json", function(data){
+            notification(datacollection + " loaded!");
+            callback(data);
+        });
+    }
+
+    /*
+     * Display a self removing notification message
+     */
+    var notification = function(message){
+        let notification = $("<div>");
+        notification.addClass("notification");
+        notification.text(message);
+        $("#notifications").append(notification);
+        setTimeout(function(){
+            notification.animate({height:"0px"}, 250);
+            notification.delay(250, function(){notification.remove();});
+        }, 2000);
+    }
+
+    /*
+     * Query a selected data collection
+     */
+    var showDataCollection = function(clientid, datacollection){
+
+    }
+
+    /*
+     * Populate the browser
+     */
+    $("#datacollections").DataCollectionTree({
+        clients:["11008", "10132"],
+        getDataCollections:getDataCollections,
+        getDataCollectionDescription:getDataCollectionDescription
+    });
+
 })
